fe4f178535a80a3b989010af95e9d43de598392b
Melvin Suter <melvin@suter.dev>
2024-05-30 09:45:38 +0200

    fix

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$CRON_ENABLED" == "true" ]] ; then
-    crontab -l | { cat; echo "$CRON_SCHEDULE root bash /app/export.sh"; } | crontab -
+    crontab -l | { cat; echo "$CRON_SCHEDULE bash /app/export.sh"; } | crontab -
     crond -f
 else
     bash /app/export.sh
